74e542b71e4ddf99b01ee1bc8dabea595c15fb56
Danylo Okhrin <danylo.okhrin@gmail.com>
2022-03-06 17:22:57 +0100

    added server

--- js/js.js
+++ js/js.js
@@ -1,6 +1,14 @@
 const jsonld = require('jsonld');
 const { getInitialContext } = require('jsonld/lib/context');
 
+//const puppeteer = require('puppeteer')
+//const {spawn} = require('child_process');
+const axios = require('axios');
+
+var quads;
+var turtle;
+var rdfXml;
+
 //TODO https://github.com/digitalbazaar/jsonld.js Dropdown with different view options
 //TODO Build SVG in external python file/javascript
 //TODO https://github.com/dherault/semantic-graphql#resolvers
@@ -17,7 +25,7 @@ document.getElementById('btn1').addEventListener("click", async function () {
 });
 
 
-document.getElementById('form1').onsubmit = function(e){
+document.getElementById('form1').onsubmit = function (e) {
     e.preventDefault();
     getMovie();
 }
@@ -30,6 +38,32 @@ async function getMovie() {
         document.getElementById('graph_scatter_1').innerHTML = "Please type in a Movie Name";
         return;
     }
+
+    quads = await getQuads(filmTitle);
+    
+    axios.get('http://127.0.0.1:8080/' + filmTitle)
+        .then(res => {
+            console.log(`statusCode: ${res.status}`)
+            console.log(res)
+            rdfXml = res.data.xml
+            turtle = res.data.turtle
+            console.log(quads)
+            console.log(rdfXml)
+            console.log(turtle)
+        })
+        .catch(error => {
+            console.error(error)
+        })
+ 
+    document.getElementById('n-quads_container').innerHTML = "<p style=\"white-space: pre-wrap\">" + quads + "</p>";
+    d3.json(res, (err, data) => {
+        if (err) return console.warn(err);
+        d3.jsonldvis(data, 'graph_scatter_global_2', { w: 800, h: 600, maxLabelWidth: 250 });
+    });
+}
+
+//gets JSON from imdb and converts it to quads
+async function getQuads(filmTitle) {
     const url = "http://www.omdbapi.com/?apikey=faeed64f&t=" + filmTitle;
     console.log(url)
     let response = await fetch(url)
@@ -44,20 +78,16 @@ async function getMovie() {
     let res = JSON.stringify(response).toString();
     console.log(res)
     console.log(response);
+
     quads = await jsonld.toRDF(response, { format: 'application/n-quads' });
     quads = format_quads(quads, filmTitle);
-    document.getElementById('n-quads_container').innerHTML = "<p style=\"white-space: pre-wrap\">" + quads + "</p>"; //TODO Check ob automatischer Zeilenumbruch verfügbar
-    d3.json(res, (err, data) => {
-        if (err) return console.warn(err);
-        d3.jsonldvis(data, 'graph_scatter_global_2', { w: 800, h: 600, maxLabelWidth: 250 });
-    });
+    return quads;
+
 }
-
-
-
-function format_quads(text, filmTitle){
-    text = text.replace(/_:b0/gi, '&lt;' + filmTitle + '&gt;');   
-    text = text.replace(/</gi,'&lt;');
-    text = text.replace(/>/gi,'&gt;');
+//is used in getQuads(filmTitle)
+function format_quads(text, filmTitle) {
+    text = text.replace(/_:b0/gi, '&lt;' + "http://schema.org/Movie" + '&gt;');
+    text = text.replace(/</gi, '&lt;');
+    text = text.replace(/>/gi, '&gt;');
     return text;
 }
---
+++ js/server.js
@@ -0,0 +1,122 @@
+const http = require('http')
+const puppeteer = require('puppeteer')
+const jsonld = require('jsonld');
+const { getInitialContext } = require('jsonld/lib/context');
+
+const port = process.env.PORT || 8080
+
+var QUADS = "";
+var xml = "";
+var turtle = "";
+
+const server = http.createServer((req, res) => {
+    if (req.url == "/" || req.url == "") {
+        //do nothing
+        //return empty?
+    } else {
+        var title = req.url.substring(1);
+        console.log("Recieved Movie: " + title);
+        console.log("Starting formatting...")
+        start(title).then(answer);
+    }
+
+    function answer() {
+        res.statusCode = 200
+        res.setHeader('Content-Type', 'application/json')
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
+        res.setHeader('Access-Control-Allow-Credentials', true); // If needed
+        res.write(JSON.stringify({ 
+            'quads': QUADS,
+            'xml': xml,
+            'turtle': turtle
+     }))
+        res.end()
+    }
+})
+
+server.listen(port, "127.0.0.1", () => {
+    console.log(`Server listening to: http://${server.address().address}:${server.address().port}`)
+})
+
+
+async function start(filmTitle) {
+    quads = await getQuads(filmTitle);
+    await convert(quads, 'rdfxml');
+    console.log("###################    Quads   #########################");
+    console.log(quads);
+    console.log("###################    XML     #########################");
+    console.log(xml)
+    console.log("################### Turtle     #########################");
+    console.log(turtle);
+    console.log("##################################################");
+}
+
+//gets JSON from imdb and converts it to quads
+async function getQuads(filmTitle) {
+    const url = "http://www.omdbapi.com/?apikey=faeed64f&t=" + filmTitle;
+    let response = await fetch(url)
+        .then(function (response) {
+            if (response.status >= 400) {
+                throw new Error("Bad response from server");
+            }
+            return response.json();
+        });
+
+    response["@context"] = "https://schema.org/";
+    let res = JSON.stringify(response).toString();
+
+    var quads = await jsonld.toRDF(response, { format: 'application/n-quads' });
+    //quads = await format_quads(quads, filmTitle);
+    quads = quads.replace(/_:b0/gi, '<http://schema.org/Movie>');
+    QUADS = format_quads(quads, filmTitle);
+    return quads;
+
+}
+//is used in getQuads(filmTitle)
+function format_quads(text, filmTitle) {
+    text = text.replace(/_:b0/gi, '&lt;' + "http://schema.org/Movie" + '&gt;');
+    text = text.replace(/</gi, '&lt;');
+    text = text.replace(/>/gi, '&gt;');
+    return text;
+}
+
+
+
+async function convert(quads, type) {
+    const browser = await puppeteer.launch({ headless: true });
+    const page = await browser.newPage();
+    await page.goto('https://www.easyrdf.org/converter');
+
+
+    //put in the data
+    await page.type("#data", quads);
+
+    //change dropdown  to xml
+    if (type == 'rdfxml') {
+        await Promise.all([page.select("#out", "rdfxml")])
+    }
+
+    //submit form
+    await page.evaluate(() => {
+        document.querySelector('input[type=submit]').click();
+    });
+
+    //waiting for page to process
+    await page.waitForTimeout(1000);
+
+    //read result
+    const data = await page.$eval('#result', el => el.innerText);
+
+    if (type == 'rdfxml') {
+        xml = data;
+    } else {
+        turtle = data;
+    }
+    await browser.close();
+
+    if (type != 'turtle') {
+        await convert(quads, 'turtle')
+    }
+}
